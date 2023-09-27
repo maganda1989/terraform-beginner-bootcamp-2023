921f76d9eda764202cbeef373b5b66a780b58a08
Orhan Tanimak <orhan.tanimak@hotmail.se>
2023-09-27 17:47:14 +0000

    21-reconstruct-root-module

--- main.tf
+++ main.tf
@@ -1,46 +1,18 @@
-terraform {
-   cloud {
-    organization = "terraform-beginner-bootcamp-2023-orhan"
-
-    workspaces {
-      name = "terra-house"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-      aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-
-}
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
-#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  lower            = true
-  upper            = false
-  length           = 32
-  special          = false
-
+  lower = true
+  upper = false
+  length   = 32
+  special  = false
 }
 
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/example-bucket-policies.html
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
 
-}
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+   cloud {
+    organization = "terraform-beginner-bootcamp-2023-orhan"
+
+    workspaces {
+      name = "terra-house"
+    }
+  }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+}
+provider "random" {
+  # Configuration options
+}
