55dc430b4ccb7e99608d43cf38a1d38513b5bc90
maganda1989 <119958041+maganda1989@users.noreply.github.com>
2023-09-27 21:03:05 +0200

    #27(23) Configuration drift (#28)

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 terraform {
-   cloud {
-    organization = "terraform-beginner-bootcamp-2023-orhan"
+#   cloud {
+#    organization = "terraform-beginner-bootcamp-2023-orhan"
 
-    workspaces {
-      name = "terra-house"
-    }
-  }
+#    workspaces {
+#      name = "terra-house"
+#   }
+#  }
   required_providers {
     random = {
       source = "hashicorp/random"
