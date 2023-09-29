76e942833145dc8b192da825c69995dd3b6adfa1
maganda1989 <119958041+maganda1989@users.noreply.github.com>
2023-09-30 00:24:28 +0200

    #32 only upload index.html if a content version has changed. (#36)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -12,8 +12,8 @@ variable "bucket_name" {
   type        = string
 
   validation {
-    condition = (
-      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
+    condition     = (
+      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
       can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
     )
     error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
@@ -37,5 +37,15 @@ variable "error_html_filepath" {
   validation {
     condition     = fileexists(var.error_html_filepath)
     error_message = "The provided path for error.html does not exist."
+  }
+}
+
+variable "content_version" {
+  description = "The content version. Should be a positive integer starting at 1."
+  type        = number
+
+  validation {
+    condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
+    error_message = "The content_version must be a positive integer starting at 1."
   }
 }
