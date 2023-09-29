f65bb1e6ff585e5e97d413987640b837d8bd2cbc
Orhan Tanimak <orhan.tanimak@hotmail.se>
2023-09-29 19:24:20 +0000

    #31 (27) enabled Static website hosting, uploaded index and error file via terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ terraform {
   #backend "remote" {
   #  hostname = "app.terraform.io"
   #  organization = "terraform-beginner-bootcamp-2023-orhan"
-
   #  workspaces {
   #    name = "terra-house"
   #  }
@@ -20,4 +19,6 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }
