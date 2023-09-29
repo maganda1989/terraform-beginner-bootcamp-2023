e080aebb5d36162b7fa4c1157aa4abd8dadeeca6
maganda1989 <119958041+maganda1989@users.noreply.github.com>
2023-09-29 20:25:15 +0200

    #29 refactor S3 bucket into the new terrahouse_aws module (#30)

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "terraform-beginner-bootcamp-2023-orhan"
 
-  tags = {
-    UserUuid = var.user_uuid
-  }
+  #  workspaces {
+  #    name = "terra-house"
+  #  }
+  #}
+ #   cloud {
+#    organization = "terraform-beginner-bootcamp-2023-orhan"
+#    workspaces {
+#      name = "terra-house"
+#   }
+#  }
+
+}
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
