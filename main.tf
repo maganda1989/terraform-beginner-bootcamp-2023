b6ede42910050b7f7770e285ee2615a43708b373
maganda1989 <119958041+maganda1989@users.noreply.github.com>
2023-10-08 22:48:54 +0200

    46 terratowns provider (#48)
    * #46 terraform provider
    
    * #47 setup skeleton for resource

--- main.tf
+++ main.tf
@@ -1,26 +1,38 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   #backend "remote" {
   #  hostname = "app.terraform.io"
-  #  organization = "terraform-beginner-bootcamp-2023-orhan"
+  #  organization = "ExamPro"
+
   #  workspaces {
-  #    name = "terra-house"
+  #    name = "terra-house-1"
   #  }
   #}
- #   cloud {
-#    organization = "terraform-beginner-bootcamp-2023-orhan"
-#    workspaces {
-#      name = "terra-house"
-#   }
-#  }
+  #cloud {
+  #  organization = "ExamPro"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
 
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="8b5322ea-41bf-4920-8ebc-dee2501bdfcc" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
+
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  index_html_filepath = var.index_html_filepath
+#  error_html_filepath = var.error_html_filepath
+#  content_version = var.content_version
+#}
