76d876267a59a2a369f13f047a98ce7654023ef0
maganda1989 <119958041+maganda1989@users.noreply.github.com>
2023-10-16 12:40:05 +0200

    46 terratowns provider (#50)
    * #46 terraform provider
    
    * #47 setup skeleton for resource
    
    * #46 fix skeleton and custom provider
    
    * #46 2.4 small fixes and test of mock env

--- main.tf
+++ main.tf
@@ -7,23 +7,23 @@ terraform {
   }
   #backend "remote" {
   #  hostname = "app.terraform.io"
-  #  organization = "ExamPro"
+  #  organization = "terraform-beginner-bootcamp-2023-orhan"
 
   #  workspaces {
-  #    name = "terra-house-1"
+  #    name = "terra-house"
   #  }
   #}
   #cloud {
-  #  organization = "ExamPro"
+  #  organization = "terraform-beginner-bootcamp-2023-orhan"
   #  workspaces {
-  #    name = "terra-house-1"
+  #    name = "terra-house"
   #  }
   #}
 
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="8b5322ea-41bf-4920-8ebc-dee2501bdfcc" 
   token="2e51a260-58cd-400a-b99c-d84f5bc46ad9"
 }
@@ -36,3 +36,15 @@ provider "terratowns" {
 #  error_html_filepath = var.error_html_filepath
 #  content_version = var.content_version
 #}
+
+
+resource "terratowns_home" "home" {
+  name = "Orhan testing"
+  description = <<DESCRIPTION
+orhan testing this site test
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "d7jmypizpnr8.cloudfront.net"
+  town = "cooker-cove"
+  content_version = 1
+}
